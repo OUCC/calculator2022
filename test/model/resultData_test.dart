0b2f90a8de465ab09035a52abcc7c9fe161f7f70
Arei1126 <103196861+Arei1126@users.noreply.github.com>
2022-06-12 11:37:43 +0900

    #23 テストの追加と名前の変更

---
+++ test/model/resultData_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:calculator2022/model/resultData.dart';
+import "dart:convert";
+
+void main() {
+
+  test("fromJson_test",(){
+  Map<String, dynamic> json = {
+  "id":0,
+  "formula":"fo-myura",
+  "result":0.3232,
+  "createdDate": DateTime(2001,05,24,12,34)
+};
+  var jsd = new ResultData.fromJson(json);
+
+  print(jsd.formula);
+  expect(jsd.formula, "fo-myura");
+  });
+///////////////////
+  test("toJson_test",(){
+    ResultData jsd = new ResultData(1,"fo-myura",0.3,DateTime(2001,05,24,12,34));
+    Map<String, dynamic> json = {
+  "id":1,
+  "formula":"fo-myura",
+  "result":0.3,
+  "createdDate": DateTime(2001,05,24,12,34)
+};
+  var jsdJson = jsd.toJson();
+
+  print(jsdJson); 
+  expect(jsdJson, json);
+  });
+}
