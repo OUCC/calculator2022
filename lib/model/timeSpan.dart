7fead98375b436669870da406a4f1441e3521a3a
hurikku <hrk5432@gmail.com>
2022-06-16 20:58:40 +0900

    parse以外の試作

---
+++ lib/model/timeSpan.dart
@@ -0,0 +1,34 @@
+class TimeSpan {
+  int _day = 0;
+  int _hour = 0;
+  int _minute = 0;
+  double _second = 0;
+  int _milliSec = 0;
+
+  TimeSpan({int day = 0, int hour = 0, int minute = 0, double second = 0}) {
+    _day = day;
+    _hour = hour;
+    _minute = minute;
+    _second = second;
+    _milliSec = (_second * 1000).toInt();
+    //カプセル化しつつコンストラクタ外で使うための処理です、効率が悪いかもしれません
+    //secondは他で使うときintにキャストする関係上少数第四位以下の情報はsecondにしか保存されません
+  }
+
+  void fromMilliSec(int fromMS) {
+    _milliSec += fromMS;
+  }
+
+  void FMS_final() {
+    final int fullMilliSec =
+        _day * 86400000 + _hour * 3600000 + _minute * 60000 + _milliSec;
+  }
+
+  TimeSpan.FMS_final();
+
+  int get day => _day;
+  int get hour => _hour % 24;
+  int get minute => _minute % 60;
+  int get second => _milliSec ~/ 1000 % 60;
+  int get milliSec => _milliSec % 1000;
+}
---
+++ test/model/timeSpan_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:calculator2022/model/timeSpan.dart';
+
+void main() {
+  final timeSpan = TimeSpan(hour: 3, minute: 3, second: 3.3);
+  test("sample-test", () {
+    final day = timeSpan.day;
+    expect(day, 3);
+  });
+}
