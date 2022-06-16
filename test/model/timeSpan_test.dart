b3fc5ba840c2dbfc0d1d300bc8a75c7621887379
hurikku <71816312+hurikku@users.noreply.github.com>
2022-06-16 22:30:40 +0900

    Update timeSpan_test.dart

--- test/model/timeSpan_test.dart
+++ test/model/timeSpan_test.dart
@@ -5,6 +5,6 @@ void main() {
   final timeSpan = TimeSpan(hour: 3, minute: 3, second: 3.3);
   test("sample-test", () {
     final day = timeSpan.day;
-    expect(day, 3);
+    expect(day, 0);
   });
 }
