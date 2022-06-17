cea5275ae4214b4175ff2dd617e3c92c441dbac3
yuyu31 <84178710+yuyu31@users.noreply.github.com>
2022-06-17 19:04:40 +0900

    #1計算履歴を出力する部分追加。キーボードUI変更。キーボード切替用の画面遷移の基礎実装。

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:async'; //Stream用
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-//https://2357developnote.blogspot.com/2020/05/flutter-calculator1.htmlを参考。
+//https://2357developnote.blogspot.com/2020/05/flutter-calculator1.htmlを参考。雛形作成。
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,15 +14,43 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             body: Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        TextField(), //メモリ部と数式表示部
-        Keyboard(), //入力部
+      children: <Widget>[
+        MemoryField(), //メモリ部
+        TextField(), //数式表示部
+        Keyboard_1(), //入力部1枚目
       ],
     )));
   }
 }
 
-//メモリと数式表示。TextFiledとTextFieldは誤字ではなく別物。
+//メモリ部。
+class MemoryField extends StatefulWidget {
+  _MemoryFiledState createState() => _MemoryFiledState();
+}
+
+//メモリ部の続き。とりあえず数式表示部と同じ構造にするのがいいかな？
+class _MemoryFiledState extends State<MemoryField> {
+  String _expression = '1+1=2';
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 1,
+        child: Container(
+          color: const Color(0xff87cefa),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              _expression,
+              style: TextStyle(
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+//数式表示部。TextFiledとTextFieldは誤字ではなく別物。
 class TextField extends StatefulWidget {
   _TextFiledState createState() => _TextFiledState();
 }
@@ -47,7 +76,7 @@ class _TextFiledState extends State<TextField> {
         flex: 1,
         child: Container(
           child: Align(
-            alignment: Alignment.centerRight,
+            alignment: Alignment.centerRight, //右詰め
             child: Text(
               _expression,
               style: TextStyle(
@@ -60,15 +89,81 @@ class _TextFiledState extends State<TextField> {
 
   static final controller = StreamController<String>();
 
-  ///Stream生成
+  //Stream生成
   @override
   void initState() {
     controller.stream.listen((event) => _UpdateText(event));
   }
 }
 
-//キーボード
-class Keyboard extends StatelessWidget {
+//キーボード1枚目
+class Keyboard_1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        flex: 5, //高さの割合
+        child: Column(children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              // （1） 指定した画面に遷移する
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      // （2） 実際に表示するページを指定する
+                      builder: (context) => Keyboard_2()));
+            },
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+            ),
+            child: Text('キーボード切替'),
+          ),
+          Expanded(
+              child: Center(
+                  child: Container(
+            color: const Color(0xff87cefa),
+            child: GridView.count(
+              childAspectRatio: 1.5, // グリッドのサイズが正方形いっぱいにならないように。大きいほどサイズが小さくなる。
+              crossAxisCount: 4, //一行に4個表示
+              mainAxisSpacing: 2.0,
+              crossAxisSpacing: 2.0,
+              children: [
+                '√',
+                'π',
+                '^',
+                'rd', //乱数
+                'AC',
+                '(',
+                ')',
+                '÷',
+                '7',
+                '8',
+                '9',
+                '×',
+                '4',
+                '5',
+                '6',
+                '-',
+                '1',
+                '2',
+                '3',
+                '+',
+                '0',
+                '.',
+                '消',
+                '=',
+              ].map((key) {
+                return GridTile(
+                  child: Button(key),
+                );
+              }).toList(),
+            ),
+          )))
+        ]));
+  }
+}
+
+//キーボード2枚目
+class Keyboard_2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -82,13 +177,13 @@ class Keyboard extends StatelessWidget {
             mainAxisSpacing: 3.0,
             crossAxisSpacing: 3.0,
             children: [
-              '√',
-              'π',
-              '^',
-              'ページ切替',
+              'sin',
+              'cos',
+              'tan',
+              ':', //時間用
               'AC',
-              '(',
-              ')',
+              'e',
+              '//', //抵抗並列用演算子
               '÷',
               '7',
               '8',
@@ -124,14 +219,11 @@ class Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         //googleの円ボタン//
-        child: ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-      ),
+        child: TextButton(
       child: Center(
         child: Text(
           _key,
-          style: TextStyle(fontSize: 30.0),
+          style: TextStyle(fontSize: 32.0, color: Colors.blueGrey),
         ),
       ),
       onPressed: () {
