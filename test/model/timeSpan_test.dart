16bc47548ebb886f2f3eb16d8f0ea796e12ee622
hurikku <hrk5432@gmail.com>
2022-06-18 13:04:37 +0900

    テスト追加

--- test/model/timeSpan_test.dart
+++ test/model/timeSpan_test.dart
@@ -2,7 +2,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:calculator2022/model/timeSpan.dart';
 
 void main() {
-  test("sample-test", () {
+  test("int-tests", () {
+    final int d = getSecFromTime(3723.4);
+    expect(d, 3);
+    final int e = getMinFromTime(3723.4);
+    expect(e, 2);
+    final int f = getHourFromTime(3723.4);
+    expect(f, 1);
+  });
+  test("double-tests", () {
+    final double g = getLimSecFromTime(3723.4);
+    expect(g, 3.4);
+    final double h = getFullMinFromTime(3723.3);
+    expect(h, 62.055);
+    final double i = getFullHourFromTime(3723.3);
+    expect(i, 1.03425);
+  });
+  test("doubleToString", () {
     final String a = getStrFromTime(3723.4);
     expect(a, "1:2:3.4");
   });
@@ -11,7 +27,7 @@ void main() {
     expect(b, 3723.4);
   });
   test("StringToDouble2", () {
-    final double b = getTimeFromStr("1.0002:::");
-    expect(b, 216043.2);
+    final double c = getTimeFromStr("1.0002:::");
+    expect(c, 216043.2);
   });
 }
