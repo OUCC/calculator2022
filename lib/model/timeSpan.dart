0f2798277c22fbfb5046fda9bd426aa75e8263b3
hurikku <hrk5432@gmail.com>
2022-06-18 11:13:30 +0900

    doubleToStringの追加

--- lib/model/timeSpan.dart
+++ lib/model/timeSpan.dart
@@ -10,14 +10,29 @@ int getHourFromTime(double sec) {
   return sec ~/ 3600;
 }
 
+double castNum(double num) {
+  const int cast = 1000000;
+  return (num * cast).toInt() / cast;
+  //(cast ^ -1)未満をキャストするための関数です(現在はマイクロ)
+}
+
 double getLimSecFromTime(double sec) {
-  return sec % 60;
+  return castNum(sec % 60);
 }
 
 double getFullMinFromTime(double sec) {
-  return sec / 60;
+  return castNum(sec / 60);
 }
 
 double getFullHourFromTime(double sec) {
-  return sec / 3600;
+  return castNum(sec / 3600);
 }
+
+String getStrFromTime(double sec) {
+  final int hour = getHourFromTime(sec);
+  final int min = getMinFromTime(sec);
+  final double limSec = getLimSecFromTime(sec);
+
+  return hour.toString() + ":" + min.toString() + ":" + limSec.toString();
+}
+
--- test/model/timeSpan_test.dart
+++ test/model/timeSpan_test.dart
@@ -3,7 +3,7 @@ import 'package:calculator2022/model/timeSpan.dart';
 
 void main() {
   test("sample-test", () {
-   
+    final String a = getStrFromTime(3723.4);
+    expect(a, "1:2:3.4");
   });
-
 }
